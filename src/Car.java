edf02d0ee81fe0eacc9ab9a9dfc8c7393ca20384
limeKIWI <pop78978@gmail.com>
2022-08-11 09:46:29 +0900

    주석수정

--- src/Car.java
+++ src/Car.java
@@ -6,8 +6,8 @@ public abstract class Car {
     protected int totalFee; // 벌어들인요금
     private int oilGage; // 주유량
     private int speed; // 현재속도
-//    String status; // 현재상태
 
+    // 현재상태
     enum stat {
         운행중, 일반, 차고지행, 운행불가
     }
@@ -30,11 +30,10 @@ public abstract class Car {
 
     // 달릴수 있는 기름양인지 확인
     public boolean isValidOil(boolean alert) {
-        if (oilGage < 10) { // 주유량이 10미만이면
-            if (alert)
+        if (oilGage < 10 && alert) { // 주유량이 10미만인데 알림이 필요할경우
                 System.out.println("!!!주유필요!!!");
         }
-        return (oilGage < 10) ? false : true;
+        return (oilGage < 10) ? false : true;   // 주유량이 10미만이면 false 아니면 true반환
     }
 
     // 주유량 출력
@@ -46,11 +45,11 @@ public abstract class Car {
     public int getSpeed() { return speed; }
 
     // 주유량 변경
-    public void setOilGage(int amount) { oilGage = (amount + oilGage < 0) ? 0 : amount + oilGage;  } // 연료의 최소량은 0 이하로 떨어지지않는다.
+    public void setOilGage(int amount) { oilGage = Math.max(amount + oilGage, 0);  } // 연료의 최소량은 0 이하로 떨어지지않는다.
 
     // 속도저장
     public void setSpeed(int newSpeed) {
-        speed = (speed + newSpeed < 0) ? 0 : speed + newSpeed; // 속도가 0이하로는 떨어지지 않는다.
+        speed = Math.max(speed + newSpeed, 0); // 속도가 0이하로는 떨어지지 않는다.
         System.out.println("현재 속도 : " + speed);
     }
 
@@ -63,6 +62,7 @@ public abstract class Car {
     // 상태 변경
     public abstract void switchStatus(String str);
 
+
     // 속도 변경
     public abstract void changeSpeed(int newSpeed);
 
@@ -72,4 +72,11 @@ public abstract class Car {
     // 차량 상태 출력
     public abstract void printStatus();
 
+}
+
+// 결제를 할수있는가?
+interface PayAble {
+    void pay(); // 결제하기
+
+    int calcPay();  // 요금계산하기
 }
